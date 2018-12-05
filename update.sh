2132bc0c48164016f283b3c9c3e430dd0b9383d7
alexandria16 <45629940+alexandria16@users.noreply.github.com>
2018-12-05 13:52:31 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,46 @@
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/musket/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/muskettool
+
+clear
+
+mkdir muskettoll
+
+clear
+
+cd muskettool
+
+clear
+
+git clone https://github.com/alexandria16/musket.git
+
+clear
+
+cd musket
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
